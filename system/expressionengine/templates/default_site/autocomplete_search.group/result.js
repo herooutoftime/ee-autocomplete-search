18db6c4796f8b7e0382f76a539e5c5a1b2d4b08c
Andreas Bilz <anti@herooutoftime.com>
2015-12-18 11:12:19 +0100

    Add loading spinner, add channel title (i18n), several improvements

--- system/expressionengine/templates/default_site/autocomplete_search.group/result.js
+++ system/expressionengine/templates/default_site/autocomplete_search.group/result.js
@@ -2,11 +2,13 @@
 {exp:search:search_results}
 {
     "channel": "{channel_title}",
+    "channel_i18n": "{exp:transcribe:replace name="channel-{channel_name}"}",
     "value": "{exp:autocomplete_search:clean}{title}{/exp:autocomplete_search:clean}",
     "url": "{page_url}",
     "uri": "{auto_path}",
     "crumb": "{exp:structure:breadcrumb uri="/{auto_path}/" here_as_title="yes"}",
-    "tokens": ["{exp:autocomplete_search:split value="{title}" delimiter=" " join=","}"]
+    "tokens": ["{exp:autocomplete_search:split value="{title}" delimiter=" " join=","}"],
+    "thumb": "{images}"
 }{if count != total_results},{/if}
 {/exp:search:search_results}
 ]
--- themes/third_party/autocomplete_search/js/autocomplete_search.js
+++ themes/third_party/autocomplete_search/js/autocomplete_search.js
@@ -1,36 +1,56 @@
-jQuery(document).ready(function($) {
+String.prototype.trunc = String.prototype.trunc ||
+  function(n){
+      return (this.length > n) ? this.substr(0,n-1)+'&hellip;' : this;
+  };
 
-    var bestGuess = new Bloodhound({
+jQuery(document).ready(function($) {
+  var BENE = {};
+
+  BENE.Typeahead = {
+    bestGuess: new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         remote: {
             url: '/autocomplete_search?keywords=%QUERY'
             ,wildcard: '%QUERY'
         }
-    });
+    }),
 
-    $('#keywords').typeahead({
-        hint: true
-        ,highlight: true
-        ,minLength: 2
-    },
-    {
-      name: 'best-guess'
-      ,display: 'value'
-      ,source:  bestGuess
-      ,limit: 25
-      ,templates: {
-          suggestion: function(e) {
-              return '<div>'+ e.value + '</div>';
-          },
-          empty: function(ctx) {
-            console.log(ctx);
-            return ctx.query;
-          }
-      }
-    });
-    $('#keywords').bind('typeahead:select', function(ev, suggestion) {
-      console.log(suggestion);
-      window.location.href = suggestion.url;
-    });
+    init: function() {
+      $('#keywords').typeahead({
+          hint: true
+          ,highlight: true
+          ,minLength: 2
+      },
+      {
+        name: 'best-guess'
+        ,display: 'value'
+        ,source:  this.bestGuess
+        ,limit: 25
+        ,templates: {
+            suggestion: function(e) {
+                return '<div>' + e.value.trunc(60) + ' <small>in ' + e.channel_i18n + '</small></div>';
+            },
+            empty: function(ctx) {
+              console.log(ctx);
+              return ctx.query;
+            }
+        }
+      })
+      .bind('typeahead:select', function(ev, suggestion) {
+        window.location.href = suggestion.url;
+      })
+      .bind('typeahead:asyncrequest', function(ev, suggestion) {
+        // console.log('Request starting');
+        $('.tt-hint').addClass('tt-input-loading');
+      })
+      .bind('typeahead:asyncreceive', function(ev, suggestion) {
+        // console.log('Request finished');
+        $('.tt-hint').removeClass('tt-input-loading');
+      });
+    }
+  }
+
+  BENE.Typeahead.init();
+
 });
